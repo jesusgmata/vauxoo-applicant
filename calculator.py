8cf7c48b5d7b99c339087006c1fcf863985cf495
jesusgmata <jesuslgmata@gmail.com>
2017-05-05 17:23:06 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
-"""
-Your module documentation here
-"""
+class calculator_class(object):
+
+    @staticmethod
+    def sum(data):
+        l = len(data) 
+        j = data[1]
+        d=0;
+        for x in range(0, l):
+      	    j = data[x]
+      	    d = d+int(j)
+
+        return d
+
+    def __init__(self):
+		    while 1:
+		    	re=calculator_class.sum(raw_input())
+		    	print "suma:",re
+                calculator_class()
 
 
-class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
-
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
